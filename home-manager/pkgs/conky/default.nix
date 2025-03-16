e515f0fa5ece1dc1343a691b04af37aa5a0bcc31
PoudaLePanda <leo.meyniel@proton.me>
2025-03-16 23:21:19 +0100

    juhb

--- home-manager/pkgs/conky/default.nix
+++ home-manager/pkgs/conky/default.nix
@@ -5,10 +5,14 @@
     conky
   ];
 
-  xdg.configFile."conky/conky.conf".source = ./conky.conf;
-  xdg.configFile."conky/conky2.conf".source = ./conky2.conf;
-  xdg.configFile."conky/mini_clock.lua".source = ./mini_clock.lua;
-  xdg.configFile."conky/Circle.png".source = ./Circle.png;
+  # xdg.configFile."conky/conky.conf".source = ./conky.conf;
+  # xdg.configFile."conky/mini_clock.lua".source = ./mini_clock.lua;
+  # xdg.configFile."conky/Circle.png".source = ./Circle.png;
+
+  # xdg.configFile."conky/conky2.conf".source = ./conky2.conf;
+
+  xdg.configFile."conky/conky3.conf".source = ./conky3.conf;
+  xdg.configFile."conky/Circle.png".source = ./box.png;
 
   # Autostart de Conky (optionnel)
   xdg.configFile."autostart/conky.desktop".text = ''
@@ -16,7 +20,8 @@
     Type=Application
     Name=Conky
     # Exec=conky --daemonize --pause=2
-    Exec=sh -c "conky --daemonize --pause=2 --config=${"$HOME"}/.config/conky/conky.conf & conky --daemonize --pause=3 --config=${"$HOME"}/.config/conky/conky2.conf"
+    # Exec=sh -c "conky --daemonize --pause=2 --config=${"$HOME"}/.config/conky/conky.conf & conky --daemonize --pause=3 --config=${"$HOME"}/.config/conky/conky2.conf"
+    Exec=sh -c "conky --daemonize --pause=2 --config=${"$HOME"}/.config/conky/conky3.conf &"
     StartupNotify=false
     Terminal=false
   '';
